82fe048005d9455b73f8bc8795ffe5dec8468b2a
FlurinBruehwilerCMI <flurin.bruehwiler@cmiag.ch>
2023-05-16 14:32:43 +0200

    added action

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,6 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-	site: `https://astro.build`,
+	site: `https://meluetta.github.io`,
+	base: `/Modul152`
 });
